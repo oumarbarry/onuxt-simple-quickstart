78cf8539a9e1f6cb9b6e2e5bb338d5cf69c44622
Oumar Barry <oumar.barry1202@gmail.com>
2023-03-22 13:14:18 +0000

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+export default defineNuxtConfig({
+  typescript: { shim: false },
+
+  experimental: { inlineSSRStyles: false },
+
+  css: ['@unocss/reset/tailwind.css'],
+
+  modules: [
+    'nuxt-icon',
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/image-edge',
+    '@nuxt/devtools',
+  ],
+
+  image: {
+    domains: ['https://images.unsplash.com'],
+    alias: { unsplash: 'https://images.unsplash.com' },
+    unsplash: {},
+  },
+
+  sourcemap: false,
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-3xl text-green-400 font-bold">
+      SIMPLE QUICKSTART
+    </h1>
+  </div>
+</template>
